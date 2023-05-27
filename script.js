3f3de2977a89b5a3cdf478a9c8c8841e57fb2f7e
shubham1upadhyay <shubhamup47@gmail.com>
2023-05-28 02:40:39 +0530

    css code updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // const  API_URL = "https://zenquotes.io/api/today";
-const API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+// const API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 // const quotesBody = document.getElementById('quotesBody');
 document.addEventListener('DOMContentLoaded', () => {
     const quotesBody = document.getElementById('quotesBody');
